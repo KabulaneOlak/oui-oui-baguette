34c206492dc4485c7c1be25f8d3e9ca823638069
KabulaneOlak <nader.c30190@gmail.com>
2024-11-19 19:04:32 +0100

    j'ai changé des trucs

--- pruduct/button.gd
+++ pruduct/button.gd
@@ -1,6 +1,31 @@
 extends Button
-@onready var Godot0_2d = $"../Godot0"
 
+#@onready var _products = [$"../Godot0", $"../Godot1", $"../Godot2", $"../Godot3", $"../Godot4"]
 
-func _on_pressed():
-	Godot0_2d.visible = not Godot0_2d.visible
+@export var _prod : Array[Node]
+
+var _last_products = ""
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	#randomize()
+
+	#var tmp_oui = rng.randi_range(0, _products.size()-1 )
+	#
+	#_products[tmp_oui].show()
+	pass
+
+func get_products():
+	var random_products = _prod[randi() % _prod.size()]
+	return random_products
+
+func _on_pressed() -> void:
+	for element in _prod:
+		element.hide()
+	
+	
+	
+	var tmp_oui = rng.randi_range(0, _prod.size()-1 )
+	#print(tmp_oui)
+	_prod[tmp_oui].show()
